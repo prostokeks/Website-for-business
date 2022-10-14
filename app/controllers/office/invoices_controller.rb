433b4bcce5ec4badb5447ed4e1fb9c975baecac1
prostokeks <tov.camz@gmail.com>
2022-10-14 10:29:17 +0300

    update

--- app/controllers/office/invoices_controller.rb
+++ app/controllers/office/invoices_controller.rb
@@ -17,6 +17,9 @@ class Office::InvoicesController < OfficeController
     @month = Month.find(params[:month_id])
     @year = Year.find(params[:year_id])
     @product = Product.find(params[:id])
+    @users = User.all
+    #@users = User.product_ids == [@product.id]
+    #@users = User.where.not(work_group: "User")
   end
 
   def new
